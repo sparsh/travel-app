46bc6ce040f6312479114a1495ad81f9b5c6c6d5
sparsh <sparshgr8@yahoo.com>
2021-12-24 04:13:04 +0530

    Removed Server side code

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const FormData = require('form-data');
-const fetch = require("node-fetch");
-const API_KEY =  process.env.API_KEY;
 const app = express();
-console.log("KEY", API_KEY);
 
 app.use(express.static('dist'))
 
@@ -24,27 +20,5 @@ app.listen(8080, function () {
 });
 
 app.get('/test', function (req, res) {
-    const text = req.query.text;
-    const formdata = new FormData();
-    formdata.append("key", API_KEY);
-    formdata.append("txt", text);
-    formdata.append("lang", 'en');  // 2-letter code, like en es fr ...
-    
-    const requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    const url = "https://api.meaningcloud.com/sentiment-2.1";
-    fetch(url, requestOptions)
-      .then(response => response.json())
-      .then((result) => {
-          console.log("Sending", result);
-          res.send(result);
-      })
-      .catch(error => {
-          console.log('error', error);
-          res.send("Could not process request at the moment, please try again later")
-    });
-
+  
 })
